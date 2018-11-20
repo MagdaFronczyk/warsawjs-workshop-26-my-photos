ea018e35831c674ae198e6b3b8c41d0d95cfcb14
magdalena fronczyk <magdalenaoliwiaf@gmail.com>
2018-11-20 12:32:02 +0100

    change search into search by clicking

--- client/app.js
+++ client/app.js
@@ -2,13 +2,14 @@ function fetchPhotos(url) {
     return fetch(url)
         .then((response) => {
             return response.json();
-        })
+        });
 }
 
 function setup() {
     fetchPhotos("/photos")
         .then((photos) => {
             render(photos);
+            handleSearchForm(photos);
             zoomPhoto(photos[0]);
         });
 }
@@ -37,13 +38,31 @@ function zoomPhoto(src) {
     printInfo(src);
 }
 
+function handleSearchForm(src) {
+    const $input = document.createElement("input");
+    const $button = document.createElement("button");
+    const $area = document.querySelector(".app");
+    $area.appendChild($input);
+    $button.setAttribute("type", "submit");
+    $button.innerText = "Search";
+    $area.appendChild($button);
+    $button.addEventListener("click", () => {
+        const value = $input.value;
+        const filteredPhotos = src.filter((el) => {
+            return el.title.toLowerCase().match(value.toLowerCase());
+        });
+        console.log(filteredPhotos);
+        render(filteredPhotos);
+    });
+}
+
 function printInfo(src) {
     removeDetails();
     const template = `
 <div>
 <h2>${src.author}</h2> 
 <p>${src.title}</p> 
-<p>${src.tags.map(e=>`#${e}`).join(", ")}<p> 
+<p>${src.tags.map(e => `#${e}`).join(", ")}<p> 
 </div>`;
     const $area = document.querySelector(".app");
     const $container = document.createElement("div");
@@ -55,7 +74,6 @@ function printInfo(src) {
 function render(photos) {
     const $area = document.querySelector(".app");
     photos.forEach((photo) => {
-        console.log(photo);
         const $image = document.createElement("img");
         $image.setAttribute("src", photo.thumb);
         $image.addEventListener("click", () => {
